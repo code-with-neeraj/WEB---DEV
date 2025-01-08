63bf18c62f9e946592f54b9db597ac9bfbafe4e8
neerajBro44 <neerajkr145518@gmail.com>
2025-01-08 22:51:14 +0530

    mongoDB with javaScript

---
+++ MONGO/index.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+
+
+
+main()
+    .then(() => {
+        console.log("connection successful");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+});
+
+const User = mongoose.model("User", userSchema);
+
+User.deleteOne({ name: "Bruce" }).then((res) => {
+    console.log(res);
+})
+
+// User.updateOne({ age: {$gt: 48} }, { age: 55 })
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// User.find({age: {$gt: 47}})
+//     .then((res) => {
+//         console.log(res);
+//     }).catch((err) => {
+//         console.log(err);
+//     });
+
+// User.findOne({age: {$gt: 47}})
+//     .then((res) => {
+//         console.log(res);
+//     }).catch((err) => {
+//         console.log(err);
+//     });
+
+// User.insertMany([
+//     { name: "Tony", email: "tony@gmail.com", age: 50 },
+//     { name: "Peter", email: "peter@gmail.com", age: 30 },
+//     { name: "Bruce", email: "bruce@gmail.com", age: 47 },
+// ]).then((res) => {
+//     console.log(res);
+// });
+
+
+
+// const user1 = new User({
+//     name: "Adam",
+//     email: "adam@yahoo.in",
+//     age: 48,
+// });
+
+// user1.save();
+
+// const user2 = new User({
+//     name: "Eve",
+//     email: "eve@yahoo.in",
+//     age: 49,
+// });
+
+// user2
+//     .save()
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
